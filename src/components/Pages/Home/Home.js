5f20e2b05b2c66a6e618f4a1b26ef5370329f85b
AhsanMustafa665 <ahsanfaisal665@gmail.com>
2022-04-30 23:54:15 +0600

    college added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Furnitures from '../Furnitures/Furnitures';
+import img1 from '../../../Assests/images/College/i-1.jpg';
+import img2 from '../../../Assests/images/College/i-2.jpg';
+import img3 from '../../../Assests/images/College/i-3.jpg';
+import img4 from '../../../Assests/images/College/i-4.jpg';
+import img5 from '../../../Assests/images/College/i-5.jpg';
+import img6 from '../../../Assests/images/College/i-6.jpg';
+import img7 from '../../../Assests/images/College/i-7.jpg';
+import img8 from '../../../Assests/images/College/i-8.jpg';
+import img9 from '../../../Assests/images/College/i-9.jpg';
+import img10 from '../../../Assests/images/College/i-10.jpg';
+import img11 from '../../../Assests/images/College/i-11.jpg';
+import img12 from '../../../Assests/images/College/i-12.jpg';
+import img13 from '../../../Assests/images/College/i-13.jpg';
+import './Home.css';
+
 
 const Home = () => {
     
     return (
+    <>
         <div>
             
-            <Banner></Banner>
-            <h2>safdkjl</h2>
+                <Banner></Banner>
+                <h1 style={{color:'rgb(122, 15, 15)',fontWeight: '900',fontSize: '2.2rem',letterSpacing: '1px'}} className='text-center mt-5'>Featured Items</h1>
+            <div  className="container college">
+        <div className="row mt-5">
+            <div className="col-4">
+                <img className="img-fluid mb-4" src={img1} alt=""/>
+                            <div className='row'>
+                                <img className="img-fluid mb-4 col-6" src={img2} alt="" />
+                                
+                            <img className="img-fluid mb-4 h-25 col-6" src={img6} alt=""/>
+                            </div>
+                            <img  className='img-fluid mb-4' src={img12} alt="" />
+                            <img className='img-fluid mb-4' src={img13} alt="" />
+                        </div>
+                        
+            <div className="col-4">
+                <img className="img-fluid mb-4" src={img3} alt=""/>
+                <img className="img-fluid mb-4" src={img4} alt=""/>
+                <img className="img-fluid mb-4" src={img5} alt=""/>
+                
+            </div>
+            <div className="col-4">
+                <img className="img-fluid mb-4" src={img7} alt=""/>
+                <img className="img-fluid mb-4" src={img8} alt=""/>
+                <img className="img-fluid mb-4" src={img9} alt=""/>
+                <div className="row">
+                    <div className="col-6">
+                        <img className="img-fluid mb-4" src={img10} alt=""/>
+                    </div>
+                    <div className="col-6"><img className="img-fluid mb-4" src={img11} alt=""/>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </div>
             <Furnitures></Furnitures>
             
-        </div>
+        
+        </>
+                        
     );
 };
 
